d32c5521eb4adda9cd5e0ce5cc5eadcdba541792
Rahul Rawat <48511603+RahulRawat-95@users.noreply.github.com>
2022-02-05 20:35:23 +0530

    Initial Commit

---
+++ Address/AddressApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressApp.swift
+//  Address
+//
+//  Created by Rahul Rawat on 05/02/22.
+//
+
+import SwiftUI
+
+@main
+struct AddressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
